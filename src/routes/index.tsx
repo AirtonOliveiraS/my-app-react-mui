be09d277acd42dc83efc41b4447efede7c8fb7d7
airtonsantos <airton.santos@2rpnet.com>
2022-05-22 19:09:54 -0300

    feat: install @mui v5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
+import { Button, ButtonGroup } from '@mui/material';
 import {Routes, Route, Navigate } from 'react-router-dom';
 
 export const AppRoutes = () =>{
     return(
   <Routes>
 
-  <Route path ="/pagina-inicial" element={
-   <div><h1> veremos se essa bagaça funciona </h1>
-     <p>Pagina Inicial</p>
-  </div> 
-  }/>
+  <Route path ="/pagina-inicial" element={<Button>teste</Button>}/>
 
   <Route path ="*" element={<Navigate to="/pagina-inicial"/>}/>
 
