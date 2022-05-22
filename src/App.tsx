5b2fcdc1dc07ba2b1cca47fd4b0f34e138382228
airtonsantos <airton.santos@2rpnet.com>
2022-05-22 18:12:02 -0300

    feat:Insert Reac-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
+import { BrowserRouter } from "react-router-dom";
 
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      testeee
-    </div>
+ <BrowserRouter>
+   <AppRoutes/>
+ </BrowserRouter>
+ 
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,17 @@
-export { };
+import {Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () =>{
+    return(
+  <Routes>
+
+  <Route path ="/pagina-inicial" element={
+   <div><h1> veremos se essa bagaça funciona </h1>
+     <p>Pagina Inicial</p>
+  </div> 
+  }/>
+
+  <Route path ="*" element={<Navigate to="/pagina-inicial"/>}/>
+
+  </Routes> 
+    );
+} 
